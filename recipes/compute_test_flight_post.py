19c11d2639f039f9742b8a1b93e3793f3a2ccd06
admin <admin@localhost>
2026-06-17 12:48:38 +0100

    Saved recipe: COMMIT_TEST.compute_test_flight_post

--- recipes/compute_test_flight_post.py
+++ recipes/compute_test_flight_post.py
@@ -12,7 +12,7 @@ flight_data_df = flight_data.get_dataframe()
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
 
-print("test")
+print("hello world")
 
 test_flight_post_df = flight_data_df # For this sample code, simply copy input to output
 
